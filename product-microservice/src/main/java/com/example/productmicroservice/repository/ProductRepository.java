e7bee35de6c66dceb50766abe995bf6b43ac6fe3
ihakalanka <akalankaih19@gmail.com>
2023-02-03 14:10:20 +0530

    added save product in service

--- product-microservice/src/main/java/com/example/productmicroservice/repository/ProductRepository.java
+++ product-microservice/src/main/java/com/example/productmicroservice/repository/ProductRepository.java
@@ -2,7 +2,10 @@ package com.example.productmicroservice.repository;
 
 import com.example.productmicroservice.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
     Product findByName(String name);
+    ResponseEntity<HttpStatus> deleteProduct(String name);
 }
---
+++ product-microservice/src/main/java/com/example/productmicroservice/service/ProductService.java
@@ -0,0 +1,25 @@
+package com.example.productmicroservice.service;
+
+import com.example.productmicroservice.entity.Product;
+import com.example.productmicroservice.message.ResourceNotFoundException;
+import com.example.productmicroservice.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+
+
+
+}
